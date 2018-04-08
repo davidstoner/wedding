fbc2056da92592e76eff107aaffb9b60dea8a583
David Stoner <david.stoner@aerialink.com>
2018-04-08 03:50:26 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const multer  = require('multer')
+const upload = multer()
+const sqlite3 = require('sqlite3')
+
+const app = express()
+
+app.use(express.static('skeleton'))
+
+app.get('/', (req, res) => {
+  res.render('/index.html')
+})
+
+app.post('/', upload.array(), (req, res) => {
+  console.log(req.body)
+  const db = new sqlite3.Database('rsvp.sqlite')
+  db.run("INSERT INTO guests (name, phone, email, attending, guests, comments) VALUES (?, ?, ?, ?, ?, ?)", req.body.name, req.body.phone, req.body.email, req.body.attending, req.body.guests, req.body.comments)
+  db.close()
+  res.send('Got a POST request.')
+})
+
+app.listen(8081, () => console.log('App listening on port 8081!'))
